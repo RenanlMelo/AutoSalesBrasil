cc150a264c13ef229a6a7e05ecf1bbdfa0c98a3e
RenanlMelo <renanlaramelo@gmail.com>
2024-03-03 13:22:38 -0300

    .env update

--- back-end/index.js
+++ back-end/index.js
@@ -1,29 +1,30 @@
-const express = require('express')
-const cors = require('cors')
-const bodyParser = require('body-parser')
-const router = require('./routes/router')
-const mongoose = require('mongoose')
-require('dotenv/config')
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const router = require("./routes/router");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
-const app = express() 
+const app = express();
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 const corsOptions = {
-    origin: '*',
-    credentials: true,
-    optionSuccessStatus: 200
-}
+  origin: "*",
+  credentials: true,
+  optionSuccessStatus: 200,
+};
 
-app.use(cors(corsOptions))
-app.use('/', router)
+app.use(cors(corsOptions));
+app.use("/", router);
 
-mongoose.connect(process.env.MONGO)
-.then(() => console.log('Connected to DB'))
-.catch(err => console.log(err))
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => console.log("Connected to DB"))
+  .catch((err) => console.log(err));
 
-const port = process.env.PORT || 4000
+const port = process.env.PORT || 4000;
 const server = app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-})
+  console.log(`Server is running on port ${port}`);
+});
