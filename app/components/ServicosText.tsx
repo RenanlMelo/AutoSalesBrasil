2ff4c19959f27219cdcad8c0868528eff0f813bc
RenanlMelo <renanlaramelo@gmail.com>
2024-06-10 13:16:05 -0300

    update servicos text interface

--- app/components/ServicosText.tsx
+++ app/components/ServicosText.tsx
@@ -51,7 +51,7 @@ export default function ServicosText({ image1, image2, image3 }: any) {
           <div className="fixed bg-black opacity-50 inset-0 z-[60]" />
         )}
 
-        <div className="py-32 px-32 pb-24 shadow-[0_30px_40px_-25px_rgba(0,0,0,1)] relative gap-y-24 flex flex-col">
+        <div className="py-32 px-32 pb-24 shadow-[0_10px_30px_0_rgba(0,0,0,.6)] relative gap-y-24 flex flex-col">
           <div className="w-full flex justify-center items-center flex-col">
             <div className="border-b-2 px-2 pb-2 border-[#1864c7] w-fit">
               <p className="text-[#8e8e8e] w-full text-xl lg:text-2xl 2xl:text-4xl font-bold tracking-tight text-center">
@@ -64,7 +64,7 @@ export default function ServicosText({ image1, image2, image3 }: any) {
           </div>
           <div className="flex justify-evenly items-center gap-y-4 rounded-xl bg-[#101010] w-full shadow-[10px_0_30px_0_rgba(250,250,250,1))]">
             <div //1º Serviço
-              className="shadow-[0_20px_20px_-10px_rgba(0,0,0,1)] hover:shadow-[0_30px_30px_-10px_rgba(0,0,0,1)] cursor-pointer rounded-2xl overflow-hidden text-zinc-300 z-10 py-6 w-full xl:w-1/4 lg:text-xl 2xl:text-2xl font-bold flex flex-col items-center justify-center gap-x-10 bg-zinc-900 hover:bg-[#222228] duration-200"
+              className="shadow-[0_20px_20px_-10px_rgba(0,0,0,.6)] hover:shadow-[0_30px_30px_-10px_rgba(0,0,0,.6)] cursor-pointer rounded-2xl overflow-hidden text-zinc-300 z-10 py-6 w-full xl:w-1/4 lg:text-xl 2xl:text-2xl font-bold flex flex-col items-center justify-center gap-x-10 bg-zinc-900 hover:bg-[#222228] duration-200"
               onClick={openInfo}
             >
               <div className="px-8 py-3 flex justify-center items-center h-1/3 ">
@@ -73,7 +73,7 @@ export default function ServicosText({ image1, image2, image3 }: any) {
             </div>
 
             <div //2º Serviço
-              className="shadow-[0_20px_20px_-10px_rgba(0,0,0,1)] hover:shadow-[0_30px_30px_-10px_rgba(0,0,0,1)] cursor-pointer rounded-2xl overflow-hidden text-zinc-300 z-10 py-6 w-full xl:w-1/4 lg:text-xl 2xl:text-2xl font-bold flex flex-col items-center justify-center gap-x-10 bg-zinc-900 hover:bg-[#222228] duration-200"
+              className="shadow-[0_20px_20px_-10px_rgba(0,0,0,.6)] hover:shadow-[0_30px_30px_-10px_rgba(0,0,0,.6)] cursor-pointer rounded-2xl overflow-hidden text-zinc-300 z-10 py-6 w-full xl:w-1/4 lg:text-xl 2xl:text-2xl font-bold flex flex-col items-center justify-center gap-x-10 bg-zinc-900 hover:bg-[#222228] duration-200"
               onClick={openInfo2}
             >
               <div className="px-8 py-3 flex justify-center items-center h-1/3 ">
@@ -82,21 +82,13 @@ export default function ServicosText({ image1, image2, image3 }: any) {
             </div>
 
             <div //3º Serviço
-              className="shadow-[0_20px_20px_-10px_rgba(0,0,0,1)] hover:shadow-[0_30px_30px_-10px_rgba(0,0,0,1)] cursor-pointer rounded-2xl overflow-hidden text-zinc-300 z-10 py-6 w-full xl:w-1/4 lg:text-xl 2xl:text-2xl font-bold flex flex-col items-center justify-center gap-x-10 bg-zinc-900 hover:bg-[#222228] duration-200"
+              className="shadow-[0_20px_20px_-10px_rgba(0,0,0,.6)] hover:shadow-[0_30px_30px_-10px_rgba(0,0,0,.6)] cursor-pointer rounded-2xl overflow-hidden text-zinc-300 z-10 py-6 w-full xl:w-1/4 lg:text-xl 2xl:text-2xl font-bold flex flex-col items-center justify-center gap-x-10 bg-zinc-900 hover:bg-[#222228] duration-200"
               onClick={openInfo3}
             >
               <div className="px-8 py-3 flex justify-center items-center h-1/3 ">
                 <h1>Plano Content</h1>
               </div>
             </div>
-          </div>
-          <div className="absolute right-0">
-            <a
-              href="https://wa.me/5515992485445"
-              className="mb-36 md:mb-0 w-auto text-lg text-zinc-300 border px-6 py-4 rounded-xl hover:text-[#25D366] hover:border-[#25D366] hover:scale-105 duration-200"
-            >
-              Entre em contato conosco via whatsapp!
-            </a>
           </div>
         </div>
       </div>
@@ -333,7 +325,7 @@ export default function ServicosText({ image1, image2, image3 }: any) {
       </AnimatePresence>
 
       <div className="w-full">
-        <div className="py-32 px-12 mx-32 border-b border-[#8e8e8e50]">
+        <div className="py-32 px-12 mx-32 border-b border-[#8e8e8e50] max-w-4xl">
           <h2 className="text-4xl text-[#8e8e8e] font-bold px-3 mb-10">
             Compra
           </h2>
@@ -359,62 +351,50 @@ export default function ServicosText({ image1, image2, image3 }: any) {
           </ul>
           <div className="w-11/12 h-px mx-auto" />
         </div>
-        
-        <div className="py-32 px-12 mx-32 border-b border-[#8e8e8e50]">
+
+        <div className="py-32 px-12 mx-32 border-b border-[#8e8e8e50] max-w-4xl">
           <h2 className="text-4xl text-[#8e8e8e] font-bold px-3 mb-10">
-            Compra
+            Venda
           </h2>
           <ul className="text-lg text-[#8e8e8e] flex gap-y-2 flex-col">
             <li>
-              Vai comprar o seu tão sonhado carro premium, mas não sabe por onde
-              começar?
+              Chegou o momento de passar o seu veículo para frente e buscar
+              novas experiências premium no mundo automotivo. Mas como fazer
+              isso?
             </li>
-            <li>Será que o carro está em boas condições?</li>
-            <li>Não sei de mecânica, posso estar entrando em uma furada?</li>
-            <li>Não tenho tempo para visitar e nem negociar, e agora?</li>
-            <li>
-              {" "}
-              Se você se identificou com
-              <strong className="font-bold"> qualquer uma </strong> dessas
-              perguntas, este serviço é para você.
-            </li>
+            <li>Não sabe o valor do seu veículo?</li>
+            <li>Não tem tempo para anunciar, negociar e apresentar o carro?</li>
+            <li>Tem medo de cair em golpes?</li>
+            <li>Cuidamos de toda esta parte burocrática para você!</li>
             <li className="my-2 px-2 text-xl text-[#8e8e8e] border-b border-[#8e8e8e] w-fit">
-              Entre em contato conosco via
-              <strong className="font-bold"> whatsapp</strong>, será um prazer
-              te ajudar!
+              Entre em contato pelo botão{" "}
+              <strong className="font-bold">abaixo</strong> para podermos lhe
+              ajudar.
             </li>
           </ul>
           <div className="w-11/12 h-px mx-auto" />
         </div>
-    
-        <div className="py-32 px-12 mx-32 border-b border-[#8e8e8e50]">
+
+        <div className="py-32 px-12 mx-32 border-b border-[#8e8e8e50] max-w-4xl">
           <h2 className="text-4xl text-[#8e8e8e] font-bold px-3 mb-10">
-            Compra
+            Plano Content
           </h2>
           <ul className="text-lg text-[#8e8e8e] flex gap-y-2 flex-col">
             <li>
-              Vai comprar o seu tão sonhado carro premium, mas não sabe por onde
-              começar?
+              É fundamental ter boas fotos para gerar alcance e interesse no
+              anúncio do seu veículo, e nós vamos te ajudar com isso caso você
+              não tenha tempo para produzir o conteúdo ou não saiba como tirar
+              as fotos e editá-las.
             </li>
-            <li>Será que o carro está em boas condições?</li>
-            <li>Não sei de mecânica, posso estar entrando em uma furada?</li>
-            <li>Não tenho tempo para visitar e nem negociar, e agora?</li>
             <li>
-              {" "}
-              Se você se identificou com
-              <strong className="font-bold"> qualquer uma </strong> dessas
-              perguntas, este serviço é para você.
-            </li>
-            <li className="my-2 px-2 text-xl text-[#8e8e8e] border-b border-[#8e8e8e] w-fit">
-              Entre em contato conosco via
-              <strong className="font-bold"> whatsapp</strong>, será um prazer
-              te ajudar!
+              Selecionamos o local onde iremos produzir um conteúdo que irá
+              destacar o seu veículo dos demais anunciados, logo depois estas
+              imagens irão receber uma edição as tornando ainda mais exclusivas
+              e por fim serão utilizadas para o seu anúncio.
             </li>
           </ul>
           <div className="w-11/12 h-px mx-auto" />
         </div>
-        
-    
       </div>
     </>
   );
